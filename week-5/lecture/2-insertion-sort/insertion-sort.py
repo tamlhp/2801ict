29017bbd1532b0e79330eb813f5b1610c7910ffe
NTT <thanhtamlhp@gmail.com>
2023-04-13 13:47:48 +1000

    sync

---
+++ week-5/lecture/2-insertion-sort/insertion-sort.py
@@ -0,0 +1,22 @@
+# Insertion sort in Python
+
+
+def insertionSort(array):
+
+    for step in range(1, len(array)):
+        key = array[step]
+        j = step - 1
+        
+        # Compare key with each element on the left of it until an element smaller than it is found
+        # For descending order, change key<array[j] to key>array[j].        
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+        
+        # Place key at after the element just smaller than it.
+        array[j + 1] = key
+        print(array)
+
+
+data = [6, 5, 3, 1, 8, 7, 2, 4]
+insertionSort(data)
